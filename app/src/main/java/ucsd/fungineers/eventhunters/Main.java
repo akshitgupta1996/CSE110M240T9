56fc7d55bb821788b43b0ca9c21593501a44831d
Kristin Agcaoili <kagcaoil@ucsd.edu>
2016-03-01 00:23:01 -0800

    Removed EventHunters_Home.java and Tried to Implement Database Calls
    - Database calls failed
    - Used log.i to see the problem
    - Removed Hunters_main because doesnt do anything

--- app/src/main/java/ucsd/fungineers/eventhunters/Main.java
+++ app/src/main/java/ucsd/fungineers/eventhunters/Main.java
@@ -1,6 +1,7 @@
 package ucsd.fungineers.eventhunters;
 
 import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,7 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -27,6 +30,7 @@ import android.widget.Button;
 public class Main extends AppCompatActivity {
 
     public static System system;
+    private Context aContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +61,30 @@ public class Main extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eventhunters_home);
+        aContext = this;
+
+        ArrayList image_details = getListData();
+        final ListView lv1 = (ListView) findViewById(R.id.UpcomingEventsHost);
+        lv1.setAdapter(new CustomListAdapter(this, image_details));
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = lv1.getItemAtPosition(position);
+                Event eventData = (Event) o;
+
+                //final Intent i = new Intent(aContext, Attendee_Event_Details.class);
+                //i.putExtra("selectedEvent", eventData);
+                Toast.makeText(Main.this, "Selected :" + " " + eventData, Toast.LENGTH_LONG).show();
+                //startActivity(i);
+            }
+        });
+
+    }
+
+
+
+
         /*---------------------------------------------*/
 
 
@@ -115,7 +143,7 @@ public class Main extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-
+/*
         Log.i("clicks", "You are in main");
 
 
@@ -132,6 +160,111 @@ public class Main extends AppCompatActivity {
         //end event example
 
     }
+*/
+
+    private ArrayList getListData() {
+
+        //test 1
+
+        ArrayList<Event> hostEventsArray = new ArrayList<Event>();
+        ArrayList<String> attendees = new ArrayList<>();
+        attendees.add("Connor");
+        attendees.add("Kristin");
+        String hostid = "12345";
+        String location = "San Diego, CA";
+        String eventNameOne = "Pentatonix Concert";
+        String eventNameTwo = "Fall Out Boy Concert";
+        String eventNameThree = "Wizard of Oz Movie";
+        String eventNameFour = "End of Quarter Party!";
+        String eventNameFive = "Halloween Dance";
+        String eventNameSix = "Study Party";
+        String eventNameSeven = "Ingress Tournament";
+        String descriptionOne = "Let's go to a concert!";
+        String descriptionTwo = "Downtown party! Let's go!";
+        String descriptionThree = "Let's go down the yellow brick road";
+        String descriptionFour = "So many people, so much dancing";
+        String descriptionFive = "There's a dance on campus to celebrate Halloween! Stay safe!";
+        String descriptionSix = "Let's destroy those midterms with a study party. Free breakfast";
+        String descriptionSeven = "Join us as we walk around La Jolla destroying Resistance portals and resonators.";
+
+        //test 2
+        GregorianCalendar calendarOne = new GregorianCalendar(2012,1,29,12,10);
+        Log.i("tophat", "Month" + calendarOne.get(Calendar.MONTH));
+        calendarOne.add(Calendar.MONTH, -1);
+        if (calendarOne.get(Calendar.AM_PM) == 1) {
+            calendarOne.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataOne = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameOne, descriptionOne, calendarOne, location);
+        hostEventsArray.add(eventsDataOne);
+
+        GregorianCalendar calendarTwo = new GregorianCalendar(2025,6,28,9,15);
+        Log.i("tophat", "Month" + calendarTwo.get(Calendar.MONTH));
+        calendarTwo.add(Calendar.MONTH, -1);
+        if (calendarTwo.get(Calendar.AM_PM) == 1) {
+            calendarTwo.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataTwo = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameTwo, descriptionTwo, calendarTwo, location);
+        hostEventsArray.add(eventsDataTwo);
+
+        GregorianCalendar calendarThree = new GregorianCalendar(1995,4,2,20,00);
+        Log.i("tophat", "Month" + calendarThree.get(Calendar.MONTH));
+        calendarThree.add(Calendar.MONTH, -1);
+        if (calendarThree.get(Calendar.AM_PM) == 1) {
+            calendarThree.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataThree = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameThree, descriptionThree, calendarThree, location);
+        hostEventsArray.add(eventsDataThree);
+
+        GregorianCalendar calendarFour = new GregorianCalendar(2010,12,20,3,30);
+        Log.i("tophat", "Month" + calendarFour.get(Calendar.MONTH));
+        calendarFour.add(Calendar.MONTH, -1);
+        if (calendarFour.get(Calendar.AM_PM) == 1) {
+            calendarFour.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataFour = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameFour, descriptionFour, calendarFour, location);
+        hostEventsArray.add(eventsDataFour);
+
+        GregorianCalendar calendarFive = new GregorianCalendar(2016,2,29,19,20);
+        Log.i("tophat", "Month" + calendarFive.get(Calendar.MONTH));
+        calendarFive.add(Calendar.MONTH, -1);
+        if (calendarFive.get(Calendar.AM_PM) == 1) {
+            calendarFive.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataFive = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameFive, descriptionFive, calendarFive, location);
+        hostEventsArray.add(eventsDataFive);
+
+        GregorianCalendar calendarSix = new GregorianCalendar(2017,8,15,11,15);
+        Log.i("tophat", "Month" + calendarSix.get(Calendar.MONTH));
+        calendarSix.add(Calendar.MONTH, -1);
+        if (calendarSix.get(Calendar.AM_PM) == 1) {
+            calendarSix.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataSix = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameSix, descriptionSix, calendarSix, location);
+        hostEventsArray.add(eventsDataSix);
+
+        GregorianCalendar calendarSeven = new GregorianCalendar(1964,7,22,22,45);
+        Log.i("tophat", "Month" + calendarSeven.get(Calendar.MONTH));
+        calendarSeven.add(Calendar.MONTH, -1);
+        if (calendarSeven.get(Calendar.AM_PM) == 1) {
+            calendarSeven.set(Calendar.AM_PM, Calendar.PM);
+        }
+        Event eventsDataSeven = new Event(attendees, hostid, RestrictionStatus.UNDER_18, Genre.MUSIC, eventNameSeven, descriptionSeven, calendarSeven, location);
+        hostEventsArray.add(eventsDataSeven);
+
+
+        /*
+        ArrayList<Event> hostEventsArray= new ArrayList<Event>();
+        try {
+            hostEventsArray = (ArrayList) System.instance.getHostingEventsByUser(System.currentUser.getUserID());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        */
+
+        return hostEventsArray;
+    }
+
+
 
     /*This is a button click method, which will activate when any button is clicked.*/
     public void button_Click(View view){
