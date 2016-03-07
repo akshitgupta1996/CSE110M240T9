5fdee519a40666411f39c348884aa7a8e9d61269
Timothy Wang <wangtim1996@gmail.com>
2016-03-06 21:49:15 -0800

    prep merge from master

--- app/src/androidTest/java/ucsd/fungineers/eventhunters/CreateEventEspressoTest.java
+++ app/src/androidTest/java/ucsd/fungineers/eventhunters/CreateEventEspressoTest.java
@@ -4,11 +4,15 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
@@ -18,11 +22,25 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @LargeTest
 public class CreateEventEspressoTest {
 
+
+
+    private String mStringToBetyped;
     @Rule
     public ActivityTestRule<CreateEvent> mActivityRule = new ActivityTestRule(CreateEvent.class);
 
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+
     @Test
-    public void listGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    public void testCreateEvent() {
+        System system = new System(mActivityRule.getActivity());
+
+        onView(withId(R.id.field_Name)).perform(typeText(mStringToBetyped));
+
+
     }
 }
